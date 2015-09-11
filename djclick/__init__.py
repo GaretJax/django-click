e4fae469bd87694e461b065918bac077f51dd498
Jonathan Stoppani <jonathan@stoppani.name>
2015-09-11 23:01:40 +0200

    Clean up init

--- djclick/__init__.py
+++ djclick/__init__.py
@@ -4,11 +4,9 @@ Support click in Django management commands.
 
 import click
 from click import *  # NOQA
-from .adapter import CommandRegistrator
+from .adapter import CommandRegistrator as command  # NOQA
 
 
 __version__ = '0.1.1'
 __url__ = 'https://github.com/GaretJax/django-click'
 __all__ = click.__all__
-
-command = CommandRegistrator
