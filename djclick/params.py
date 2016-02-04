c32a11b237a5f9e67a4299c57ea582fb23929bcd
Jonathan Stoppani <jonathan@stoppani.name>
2016-02-04 15:45:59 +0100

    Add a ModelInstance parameter

---
+++ djclick/params.py
@@ -0,0 +1,23 @@
+import click
+
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class ModelInstance(click.ParamType):
+    def __init__(self, qs):
+        from django.db import models
+
+        if isinstance(qs, type) and issubclass(qs, models.Model):
+            qs = qs.objects.all()
+        self.qs = qs
+        self.name = '{}.{}'.format(
+            qs.model._meta.app_label,
+            qs.model.__name__,
+        )
+
+    def convert(self, value, param, ctx):
+        try:
+            return self.qs.get(pk=value)
+        except ObjectDoesNotExist:
+            msg = 'could not find {} with pk={}'.format(self.name, value)
+            self.fail(msg, param, ctx)
---
+++ djclick/test/test_params.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+from djclick import params
+
+
+def test_modelinstance_init():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testprj.settings')
+
+    from testapp.models import DummyModel
+    from django.db.models.query import QuerySet
+
+    param = params.ModelInstance(DummyModel)
+    assert isinstance(param.qs, QuerySet)
+
+    qs = DummyModel.objects.all()
+    param = params.ModelInstance(qs)
+    assert param.qs is qs
+
+
+def test_convert_ok(manage):
+    assert manage('modelcmd', 'MODEL') == 'MODEL'
+
+
+def test_convert_fail(manage):
+    try:
+        manage('modelcmd', 'ND')
+    except subprocess.CalledProcessError as e:
+        lines = e.output.splitlines()
+        assert lines[0] == b'Traceback (most recent call last):'
+        for line in lines[1:-1]:
+            assert line.startswith(b'  ')
+        assert lines[-1] == (b'click.exceptions.BadParameter: '
+                             b'could not find testapp.DummyModel with pk=ND')
+        assert e.returncode == 1
+    else:
+        assert False  # NOCOV
