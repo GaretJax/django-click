ee05e8d914d8f197d55250b65982f7e2daa6623a
Timothy Allen <tallen@wharton.upenn.edu>
2020-06-12 08:11:34 -0400

    Bump version to 2.1.1.

--- djclick/__init__.py
+++ djclick/__init__.py
@@ -8,7 +8,7 @@ from .adapter import CommandRegistrator as command  # NOQA
 from .adapter import GroupRegistrator as group, pass_verbosity  # NOQA
 
 
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 __url__ = 'https://github.com/GaretJax/django-click'
 __author__ = 'Jonathan Stoppani'
 __email__ = 'jonathan@stoppani.name'
