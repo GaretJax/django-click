4ca755c8289392303bbe37b80b6bdd9ae400c487
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-08-07 11:04:51 +0300

    Apply django-upgrade fixes

--- djclick/test/testprj/testprj/urls.py
+++ djclick/test/testprj/testprj/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r"^admin/", include(admin.site.urls)),
+    path("admin/", include(admin.site.urls)),
 ]
